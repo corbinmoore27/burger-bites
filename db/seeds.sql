12be2713dfd20f086aae9dd6f5c27dd40179897a
Corbin Moore <corbin.moore27@gmail.com>
2018-04-26 14:47:40 -0600

    initial setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Guacamole Bacon Swiss');
+INSERT INTO burgers (burger_name) VALUES ('Western BBQ');
+INSERT INTO burgers (burger_name) VALUES ('Hawaiian Salmon');
